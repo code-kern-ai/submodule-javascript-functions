648b2b44a790951b029eec69e3cd590d3b44db86
Lina <lina.lumburovska@kern.ai>
2023-05-25 14:17:04 +0200

    Added missing function formatBytes

--- general.ts
+++ general.ts
@@ -100,3 +100,11 @@ export function tryParseJSON(str: string): any {
         return null;
     }
 }
+
+export function formatBytes(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return parseFloat((bytes / Math.pow(1024, i)).toFixed(dm)) + ' ' + sizes[i];
+}
