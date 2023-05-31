3383e947d8b96cf80e4d055c6c1b3d8338ab332c
Lina <lina.lumburovska@kern.ai>
2023-05-31 16:52:04 +0200

    Added text fetch wrapper

--- basic-fetch.ts
+++ basic-fetch.ts
@@ -5,7 +5,7 @@ export enum FetchType {
     DELETE = "DELETE",
 }
 
-export function jsonFetchWrapper(url: string, fetchType: FetchType, onResult?: (result: any) => void, body?: BodyInit, headers?: HeadersInit, onError?: (response: any) => void) {
+export function jsonFetchWrapper(url: string, fetchType: FetchType, onResult?: (result: any) => void, body?: BodyInit, headers?: any, onError?: (response: any) => void) {
     if (!headers) headers = {};
     headers["Content-Type"] = "application/json";
 
@@ -26,4 +26,22 @@ export function jsonFetchWrapper(url: string, fetchType: FetchType, onResult?: (
     if (onResult && !hasError) myFetch.then(result => onResult(result));
 
 
+}
+
+export function textFetchWrapper(url: string, fetchType: FetchType, onResult?: (result: any) => void, body?: BodyInit, headers?: any) {
+    if (!headers) headers = {};
+    headers["Content-Type"] = "application/json";
+
+    let myFetch: any = fetch(url, {
+        method: fetchType,
+        headers: headers,
+        body: body,
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.text();
+        });
+    if (onResult) myFetch = myFetch.then((result: any) => onResult(result));
 }
