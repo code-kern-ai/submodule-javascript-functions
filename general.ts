32ac8c117cabf047e2be4f426ca7c4527030734a
Lina <lina.lumburovska@kern.ai>
2023-09-14 11:21:45 +0200

    Percent round js function

--- general.ts
+++ general.ts
@@ -193,3 +193,15 @@ export function getUserAvatarUri(user) {
     }
     return avatarId + ".png";
 }
+
+export function percentRound(value: number | string, decimals: number, isZeroToOne: boolean = true) {
+    if (typeof value == 'number') {
+        if (isNaN(value)) return "n/a";
+        if (!isFinite(value)) return "0 %";
+        if (isZeroToOne) value *= 100;
+        const dec = 10 ** decimals;
+        return Math.round(value * dec) / dec + ' %';
+    }
+    else if (typeof value == 'undefined' || value == null) return "n/a";
+    return value;
+}
