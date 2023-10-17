4dc3219b7263bd99e03fa4da606d54de7be87c07
Lina <lina.lumburovska@kern.ai>
2023-10-17 16:19:34 +0200

    Enums and enum functions

--- enums/enum-functions.ts
+++ enums/enum-functions.ts
@@ -1,4 +1,4 @@
-import { InformationSourceType } from "./enums";
+import { DisplayGraphs, InformationSourceType, LabelSource } from "./enums";
 
 export function informationSourceTypeToString(source: InformationSourceType, short: boolean, forDisplay: boolean = true) {
     if (forDisplay) {
@@ -8,6 +8,31 @@ export function informationSourceTypeToString(source: InformationSourceType, sho
             case InformationSourceType.PRE_COMPUTED: return short ? "PC" : "Pre Computed module";
             case InformationSourceType.ZERO_SHOT: return short ? "ZS" : "Zero Shot module";
             case InformationSourceType.CROWD_LABELER: return short ? "CL" : "Crowd labeler";
+            default: return source;
+        }
+    }
+    return source;
+}
+
+export function displayGraphsTypeToString(source: DisplayGraphs) {
+    switch (source) {
+
+        case DisplayGraphs.CONFUSION_MATRIX: return "Confusion Matrix";
+        case DisplayGraphs.INTER_ANNOTATOR: return "Inter Annotator";
+        case DisplayGraphs.LABEL_DISTRIBUTION: return "Label Distribution";
+        case DisplayGraphs.CONFIDENCE_DISTRIBUTION: return "Confidence Distribution";
+        case DisplayGraphs.ALL: return "All";
+        default: return source;
+    }
+}
+
+export function labelSourceToString(source: LabelSource, forDisplay: boolean = true) {
+    if (forDisplay) {
+        switch (source) {
+            case LabelSource.MANUAL: return "Manual";
+            case LabelSource.WEAK_SUPERVISION: return "Weak Supervision";
+            case LabelSource.MODEL_CALLBACK: return "Model Callback";
+            case LabelSource.INFORMATION_SOURCE: return "Information Source";
             default: return source;
         }
     }
