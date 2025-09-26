6adb0e8900a869bb2ed1bb5d10bd7584b238b5cd
Lina <lina.lumburovska@kern.ai>
2025-09-26 09:12:58 +0200

    PR comments

--- date-parser.ts
+++ date-parser.ts
@@ -34,7 +34,7 @@ export function timeDiffCalc(dateA: any, dateB: any = Date.now()) {
     return new Date(Math.abs(dateB - dateA)).toISOString().substring(11, 19);
 }
 
-export function formatTime(s: number) {
+export function formatTimeDigitalClock(s: number) {
     const mm = Math.floor(s / 60);
     const ss = s % 60;
     return `${String(mm).padStart(2, "0")}:${String(ss).padStart(2, "0")}`;
