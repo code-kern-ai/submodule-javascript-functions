0cbc32dedd7b3cf7034876a863d0edbb5a25098f
Andrea Hrelja <151508315+andhreljaKern@users.noreply.github.com>
2025-12-02 03:19:56 +0100

    perf: privatemode deprecations update (#24)

--- constants.ts
+++ constants.ts
@@ -1,8 +1,7 @@
 export const ENGINEERING_TEAM = 'ENGINEERING_TEAM'
 
 export const MODEL_MAP_FULL_NAME = {
-    'Meta-Llama-3.3-70B': "ibnzterrell/Meta-Llama-3.3-70B-Instruct-AWQ-INT4",
-    'gemma-3-27b': "leon-se/gemma-3-27b-it-fp8-dynamic",
-    'openai-oss-120b': 'openai/gpt-oss-120b',
+    'gemma-3-27b': "gemma-3-27b",
+    'openai-oss-120b': 'gpt-oss-120b',
     'qwen3-30b (coding)': "qwen3-coder-30b-a3b",
 }
