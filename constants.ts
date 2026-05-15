60f9eeba39b3188eee0a48bfca756296c57dfeda
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2026-05-15 15:31:14 +0200

    Hotfix for depricated kimi model (#37)

--- constants.ts
+++ constants.ts
@@ -3,5 +3,5 @@ export const ENGINEERING_TEAM = 'ENGINEERING_TEAM'
 export const MODEL_MAP_FULL_NAME = {
     'gemma-4-31b': "gemma-4-31b",
     'openai-oss-120b': 'gpt-oss-120b',
-    'kimi-k2.5': "kimi-k2.5",
+    'kimi-k2.6': "kimi-k2.6",
 }
